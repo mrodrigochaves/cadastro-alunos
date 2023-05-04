b7f65406b0b1470d3fda63521e89455f64629408
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-05-03 21:24:30 -0300

    Atualizando Aluno

--- src/edu/mrodrigochaves/cadastro/Aluno/Aluno.java
+++ src/edu/mrodrigochaves/cadastro/Aluno/Aluno.java
@@ -1,40 +1,24 @@
 package edu.mrodrigochaves.cadastro.Aluno;
 
-public class Aluno {
-    private String matricula;
-    private String nome;
-    private int idade;
-    private String endereco;
-    
-    public String getMatricula() {
-        return matricula;
-    }
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-    @Override
-    public String toString() {
-        return "Aluno [matricula=" + matricula + ", nome=" + nome + ", idade=" + idade + ", endereco=" + endereco + "]";
-    }
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import edu.mrodrigochaves.cadastro.Disciplina;
+
+public class Aluno {
+    
+    private String nome;
+    private Set<Disciplina> disciplinasConcluidas = new LinkedHashSet<>();
+    private Set<Disciplina> disciplinasIncompletas = new LinkedHashSet<>();
+
+
+    public void inscreverDisciplina(Disciplina disciplina){}
+
+    public void realizarAvaliacao(){}
+
+    public void calcular_Nota(){}
+    
+    
     
 
 }
