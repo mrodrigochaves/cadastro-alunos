69cc126c736d7be2d1ae6ac25776971a8ff60318
Márcio Rodrigo <39975073+mrodrigochaves@users.noreply.github.com>
2023-05-23 09:47:11 -0300

    Update Curso.java

--- src/edu/mrodrigochaves/cadastro/Curso/Curso.java
+++ src/edu/mrodrigochaves/cadastro/Curso/Curso.java
@@ -7,27 +7,30 @@ import java.util.Set;
 import edu.mrodrigochaves.cadastro.Aluno.Aluno;
 import edu.mrodrigochaves.cadastro.Disciplina.Disciplina;
 
-public class Curso extends Disciplina{
-
+public class Curso extends Disciplina {
     
     private int cargaHoraria;
     private Set<Aluno> alunosInscritos = new HashSet<>();
 
+    // Sobrescreve o método calcular_Nota() da classe Disciplina
     @Override
     public int calcular_Nota() {
         return Nota * cargaHoraria;
     }
 
-    public Curso(){
-
+    public Curso() {
+        // Construtor padrão
     }
    
     public int getCargaHoraria() {
         return cargaHoraria;
     }
+    
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
+    
+    // Sobrescreve o método toString() da classe Object
     @Override
     public String toString() {
         return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
@@ -37,13 +40,12 @@ public class Curso extends Disciplina{
         return alunosInscritos;
     }
 
-    public void setAlunosInscritos(Set<Aluno> alunosInscritos){
+    public void setAlunosInscritos(Set<Aluno> alunosInscritos) {
         this.alunosInscritos = alunosInscritos;
     }
 
+    // Retorna uma coleção de Disciplina (método não implementado)
     public Collection<? extends Disciplina> getDisciplinas() {
         return null;
     }
-       
-    
 }
